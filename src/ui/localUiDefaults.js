3e8ec403fd7236fa04ed7922a6aeb486d333c9e2
Guido Zeuner <gzeuner@tiny-tool.de>
2026-06-19 07:48:06 +0200

    Break Local UI and MCP require cycle

---
+++ src/ui/localUiDefaults.js
@@ -0,0 +1,21 @@
+/*
+Copyright 2026 gzeuner - tiny-tool.de
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+const DEFAULT_UI_HOST = '127.0.0.1';
+const DEFAULT_UI_PORT = 4782;
+
+module.exports = {
+  DEFAULT_UI_HOST,
+  DEFAULT_UI_PORT,
+};
